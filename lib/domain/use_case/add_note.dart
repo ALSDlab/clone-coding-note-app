f37e8795aba625132537ea756a28b474f513d672
ssd72 <sungdae.yi@gmail.com>
2024-02-12 04:32:32 +0100

    add: 섹션2 노트앱. 25 Use Case 작성

---
+++ lib/domain/use_case/add_note.dart
@@ -0,0 +1,13 @@
+import 'package:clone_coding_note_app/domain/repository/note_repository.dart';
+
+import '../model/note.dart';
+
+class AddNote {
+  final NoteRepository repository;
+
+  AddNote(this.repository);
+
+  Future<void> call(Note note) async {
+    await repository.insertNote(note);
+  }
+}
