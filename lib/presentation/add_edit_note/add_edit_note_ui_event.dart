9ac695f7135d00da4d5721a6bc15f2d29b6308f5
ssd72 <sungdae.yi@gmail.com>
2024-02-12 14:54:30 +0100

    add: 섹션2 노트앱. 34 노트 수정

--- lib/presentation/add_edit_note/add_edit_note_ui_event.dart
+++ lib/presentation/add_edit_note/add_edit_note_ui_event.dart
@@ -1,9 +1,11 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'add_edit_note_ui_event.freezed.dart';
 
 @freezed
 abstract class AddEditNoteUiEvent with _$AddEditNoteUiEvent {
   const factory AddEditNoteUiEvent.saveNote() = SaveNote;
+
+  const factory AddEditNoteUiEvent.showSnackBar(String message) = ShowSnackBar;
 }
